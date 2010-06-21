2b005eed4a524833ee7021cea858f320c413e507
winterTTr <winterTTr@gmail.com>
2010-06-21 21:36:49 +0800

    use the new model/view to rewrite the file-explorer, code complete( bugs lot )

--- src/pvFileExplorer.py
+++ src/pvFileExplorer.py
@@ -1,22 +1,77 @@
 import os
+import os.path
 
-
-from pyvim.pvBase import pvBuffer , PV_BUF_TYPE_ATTACH
+from pyvim.pvBase import pvBuffer , PV_BUF_TYPE_NORMAL
 from pyvim.pvBase import pvWindow
 from pyvim.pvUtil import pvString
 
 from pyvim.pvEvent import pvEventObserver , pvKeymapEvent , PV_KM_MODE_NORMAL
 
 from pyvim.pvTree import pvTreeBuffer , pvTreeBufferObserver
-from pyvim.pvDataModel import PV_ELEMENT_TYPE_LEEF , PV_ELEMENT_TYPE_BRANCH ,  PV_ELEMENT_TYPE_ROOT
-from pyvim.pvDataModel import pvDataElement 
-from pyvim.pvDataModel import PV_BRANCH_STATUS_CLOSE , PV_BRANCH_STATUS_OPEN
+from pyvim.pvDataModel import pvModelIndex , pvAbstractModel , PV_MODEL_ROLE_DISPLAY
+
+
+class pvFileSystemNode(object):
+    def __init__( self ):
+        self.index = None
+
+        self.path = u""
+        self.children = []
+
+class pvFileSystemModel( pvAbstractModel ):
+    def __init__(self):
+        self.root = pvFileSystemNode()
+        self.root.index = pvModelIndex()
+
+    def rowCount( self ,  index ):
+        import sys
+        if sys.platform[:3] == 'win' and ( not index.isValid() ):
+            if len( self.root.children ):
+                return len( self.root.children )
+
+            import string
+            for x in string.ascii_uppercase :
+                driver_path = u'%s:\\' % x
+                if os.path.isdir( driver_path ):
+                    newIndex = self.createIndex( len( root.children ) , None )
+                    # create new node
+                    newNode = pvFileSystemNode()
+                    newNode.index = newIndex
+                    newNode.path = driver_path
+                    newIndex.data = newNode
+                    self.root.children.append( newNode )
+            return len( self.root.children )
+        else:
+            parent_node = index.data if index.isValid() else self.root
+            if len( parent_node.children ): return len( parent_node.children )
+            for each_name in os.listdir( parent_node.path ):
+                newIndex = self.createIndex( len( parent_node.children ) , None )
+                # create new node
+                newNode = pvFileSystemNode()
+                newNode.index = newIndex
+                newNode.path = os.path.join( parent_node.path , each_name )
+                newIndex.data = newNode
+                parent_node.children.append( newNode )
+
+            return len( parent_node.children )
+                    
+
+    def index( self , row , pindex ):
+        pnode = pindex.data if pindex.isValid() else self.root
+        return pnode.children[row].index
+
+    def data( self , index , role = PV_MODEL_ROLE_DISPLAY ):
+        return os.path.basename( index.data.path )
+
+    def hasChildren( self , index ):
+        return os.path.isdir( index.data.path ) 
 
 
 class pvFileExplorer( pvTreeBufferObserver ):
     def __init__( self  , target_window ):
-        self.__buffer = pvTreeBuffer()
+        self.__buffer = pvTreeBuffer( pvFileSystemModel() )
         self.__buffer.registerObserver( self )
+
         self.__target_window = target_window
 
 
@@ -25,90 +80,25 @@ class pvFileExplorer( pvTreeBufferObserver ):
         self.__buffer = None
 
 
-    def OnElementSelect( self , element ):
-        if element.type == PV_ELEMENT_TYPE_ROOT:
-            buf_no = self.__target_window.bufferid
-            if buf_no == -1 :
-                cwd = os.getcwdu() # unicode current work directory
-            else:
-                buf = pvBuffer( PV_BUF_TYPE_ATTACH )
-                buf.attach( buf_no )
-                cwd = pvString( MultibyteString = buf.name ).UnicodeString
-                if cwd == u"" :
-                    cwd = os.getcwdu() # unicode current work directory
+    def OnTreeNodeSelected( self , index ):
+        path = index.data.path
+        if os.path.isdir( s ):
+            os.chdir( path )
+        else:
+            buffer = pvBuffer( PV_BUF_TYPE_NORMAL , path )
+            buffer.showBuffer( self.__target_window )
+            buffer.detach()
 
-            if cwd == u"":
-                return
+    def OnTreeNodeExpanded( self ,index ):
+        pass
 
-            # find the exist part
-            while not os.path.exists( cwd ):
-                cwd , tail = os.path.split( cwd )
-                if tail == u'': 
-                    if cwd[-1] == '/': cwd = cwd[:-1] + '\\'
-                    break
-
-            # split into each part
-            cwd_list = []
-            while True:
-                cwd , tail = os.path.split( cwd )
-                if tail == u'': 
-                    if cwd[-1] == '/': cwd = cwd[:-1] + '\\'
-                    cwd_list.insert( 0 , pvString( UnicodeString = cwd ) )
-                    break
-
-                cwd_list.insert( 0 ,  pvString( UnicodeString = tail ) )
-
-
-            # update data model into the current status
-
-            # 1 . update base disk flag
-            import string
-            for x in string.ascii_uppercase :
-                driver_path = u'%s:\\' % x
-                if os.path.isdir( driver_path ):
-                    # create element
-                    element = pvDataElement.CreateElement(
-                                PV_ELEMENT_TYPE_BRANCH , 
-                                pvString( UnicodeString = driver_path ) , 
-                                PV_BRANCH_STATUS_CLOSE ) 
-                    # and add to data model
-                    self.__buffer.dataModel.addElement( self.__buffer.dataModel.root , element )
-                    if element.name == cwd_list[0]: 
-                        self.__buffer.dataModel.selectedElement = element
-                        cwd_list.pop(0)
-
-
-            ## 2. expand other
-            change_flag = False
-            base_path = u"" 
-            while cwd_list :
-                former_selected_element = self.__buffer.dataModel.selectedElement
-                former_selected_element.status = PV_BRANCH_STATUS_OPEN
-                base_path = os.path.join( base_path , former_selected_element.name.UnicodeString )
-                change_flag = False
-                for each_name in os.listdir( base_path ):
-                    element = pvDataElement.CreateElement(
-                            PV_ELEMENT_TYPE_BRANCH \
-                                    if os.path.isdir( os.path.join( base_path , each_name ) ) \
-                                    else PV_ELEMENT_TYPE_LEEF, 
-                            pvString( UnicodeString = each_name ) , 
-                            PV_BRANCH_STATUS_CLOSE ) 
-                    self.__buffer.dataModel.addElement( former_selected_element , element )
-                    if not change_flag and element.name == cwd_list[0] :
-                        self.__buffer.dataModel.selectedElement = element
-                        cwd_list.pop(0)
-                        change_flag = True
-
-
-
+    def OnTreeNodeCollapsed( self , index ):
+        pass
 
     def showBuffer( self , show_win ):
         self.__buffer.showBuffer( show_win )
         self.__buffer.updateBuffer()
         self.__target_window.setFocus()
-
-
-
 
 class Application( pvEventObserver ):
     def __init__( self ):
@@ -117,24 +107,19 @@ class Application( pvEventObserver ):
         self.event = pvKeymapEvent( '<m-2>' , PV_KM_MODE_NORMAL  )
         
     def OnProcessEvent( self , event ):
-        try:
-            if self.buffer is None and self.window is None :
-                current_window = pvWindow()
-                from pyvim.pvBase import pvWinSplitter , PV_SPLIT_TYPE_MOST_LEFT
-                self.window = pvWinSplitter( PV_SPLIT_TYPE_MOST_LEFT , ( 30 , -1 ) , current_window ).doSplit()
-                self.buffer = pvFileExplorer( current_window )
-                self.buffer.showBuffer( self.window )
-            else:
-                if self.window:
-                    self.window.closeWindow()
-                    self.window = None
-                if self.buffer:
-                    self.buffer.destroy()
-                    self.buffer = None
-        except:
-            import sys
-            print sys.exc_info()
-            print sys.exc_info()[2].tb_frame.f_code
+        if self.buffer is None and self.window is None :
+            current_window = pvWindow()
+            from pyvim.pvBase import pvWinSplitter , PV_SPLIT_TYPE_MOST_LEFT
+            self.window = pvWinSplitter( PV_SPLIT_TYPE_MOST_LEFT , ( 30 , -1 ) , current_window ).doSplit()
+            self.buffer = pvFileExplorer( current_window )
+            self.buffer.showBuffer( self.window )
+        else:
+            if self.window:
+                self.window.closeWindow()
+                self.window = None
+            if self.buffer:
+                self.buffer.destroy()
+                self.buffer = None
 
     def start( self ):
         self.event.registerObserver( self )
